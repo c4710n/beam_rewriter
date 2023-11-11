6938933a3a6abe15b3395ac714b7fd50f68fbd45
c4710n <c4710n@users.noreply.github.com>
2023-11-11 18:43:53 +0800

    release: bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule BeamRewriter.MixProject do
   def project do
     [
       app: @app,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
